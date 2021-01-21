9cec67bc510a177d39adee95c0304de644c5dbfa
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-01-21 17:50:07 -0300

    Commit 20 dos arquivos

--- file-reader.js
+++ file-reader.js
@@ -1,6 +1,38 @@
-const fileReader = function(e) {
-    console.log(e.target.files.length);
-    document.getElementById('counter').innerHTML = e.target.files.length;
+const getReadedFile = function(reader, item) {
+    return function() {
+        let img = document.createElement('img');
+        img.src = reader;
+        // item.innerHTML = 'Arquivo começa com"' + reader.result.subString(0, 30) + '"';
+        item.appendChild(img);
+    };
 };
 
-document.getElementById('file').addEventListener('change', fileReader());
+const fileReader = function(e) {
+    // console.log(e.target.files.length);
+    const files = e.target.files;
+    document.getElementById('counter').innerHTML = files.length;
+
+    const list = document.getElementById('list');
+
+    for (let i = 0; i < files.length; i++) {
+        let item = document.createElement('li');
+        const reader = new FileReader();
+        reader.addEventListener('load', getReadedFile(reader, item));
+        // reader.readAsText(files[i]);
+        reader.readAsDataURL(files[i]);
+        // item.innerHTML += JSON.parce(files[i]);
+        item.innerHTML += files[i].name + ' - ' + file.type;
+        list.appendChild(item);
+    }
+};
+
+const init = function() {
+    document.getElementById('file').addEventListener('change', fileReader());
+};
+
+if (window.File) {
+    alert('Recurso não suportado');
+    return;
+} else {
+    init();
+}
--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
-document.getElementById('app').innerHTML = '<h1>Hello PWA</h1>';
+document.getElementById('app').innerHTML = `<h1>Hello PWA <span class="tooltip">Esta é uma pagina PWA</span></h1>
+<div>
+    <ul>
+        <li id="usage"></li>
+        <li id="quota"></li>
+        <li id="percent"></li>
+    </ul>
+</div>
+<div>
+    <button id="startrec">Iniciar gravação</button>
+</div>
+<div id="recresult">Nenhuma gravação</div>
+`;
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
